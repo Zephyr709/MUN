10589877eb205e4395f4fb7380b4e8ed7106a592
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2023-09-18 15:52:29 -0230

    attributes Initialization

--- comp-2001/Assignment-1/creditCard.java
+++ comp-2001/Assignment-1/creditCard.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 public class CreditCard {
-
-
-
-
-
+    String customerName;
+    String accountNumber;
+    Float balance;
+    Float creditLimit;
+    Float creditAvail;
+    ArrayList transactionHistory;
     
+
+    public CreditCard(){
+
+    }
+
+
+
+
 }
